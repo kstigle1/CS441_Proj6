99df70f43e3f485719e05375ba1595b8d5d144de
stigglesworth3 <Kyle@Stigler.com>
2021-03-28 17:40:38 -0400

    untimed leaderboard created, not tested and no sorting yet

--- app/src/main/java/com/stigler/cs441_proj6/quizResults.java
+++ app/src/main/java/com/stigler/cs441_proj6/quizResults.java
@@ -3,16 +3,26 @@ package com.stigler.cs441_proj6;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class quizResults extends AppCompatActivity
 {
     TextView outOfTen;
     Singleton single;
     Button return2main;
+    EditText whatName;
+    TableLayout UTTable;
+    ArrayList<TableRow> rowList;
+    int percentRes;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,20 +33,48 @@ public class quizResults extends AppCompatActivity
         single = Singleton.getInstance();
         outOfTen = (TextView) findViewById(R.id.out10);
         return2main = (Button) findViewById(R.id.return2main);
+        whatName = (EditText) findViewById(R.id.nameBox);
+        UTTable = (TableLayout) findViewById(R.id.UTLeaders);
 
-        int percentRes = (int) (single.totalCorrect/single.questions.size())*100; //total correct is a double
+        percentRes = (int) (single.totalCorrect/single.questions.size())*100; //total correct is a double
         outOfTen.setText("Score: " + percentRes + "%");
     }
 
     public void returnToMain(View view)
     {
+        addLeader();
         Intent actAction = new Intent(this, MainActivity.class);
         startActivity(actAction);
     }
 
     public void returnLeader(View view)
     {
+        addLeader();
         Intent actAction = new Intent(this, UTLeaderboard.class);
         startActivity(actAction);
+    }
+
+    public void addLeader()
+    {
+        String pName = whatName.getText().toString();
+        if (pName.equals(""))
+        {
+            pName = "Anonymous Player";
+        }
+        TableRow lbRow = new TableRow(this);
+        TextView playerName = new TextView(this);
+        TextView playerScore = new TextView(this);
+        playerName.setText(pName);
+        playerScore.setText(percentRes + "%");
+        playerName.setTextSize(15);
+        playerName.setTextColor(Color.BLACK);
+        playerScore.setTextSize(15);
+        playerScore.setTextColor(Color.BLACK);
+        lbRow.addView(playerName, 0);
+        lbRow.addView(playerScore, 1);
+        rowList.add(lbRow);
+        whatName.setText("");
+
+        //need to sort the list somehow
     }
 }
